0f9d0ac1b0a0770bdd3947df85aa5412ec60dbb1
AverseABFun-windows <averse.abfun@gmail.com>
2023-06-28 14:20:55 +0000

    iiokerkfjdun jkjrevfxfd n vjfgv

--- cogs/textadv.py
+++ cogs/textadv.py
@@ -9,6 +9,7 @@ Version: 0.0.1
 from discord.ext import commands
 from discord.ext.commands import Context
 import discord
+import json
 
 from helpers import checks, db_manager, data
 
@@ -44,7 +45,7 @@ class TextAdventure(commands.Cog, name="textadv"):
             )
             await context.send(embed=embed)
             return
-        await db_manager.create_state(context.author.id, "start", "[]")
+        await db_manager.create_state(context.author.id, "start", '{"attack":0.5,"defense":0.5,"reload":0}')
         state = await get_state(context)
         try:
             room = data.get_room_data(state[1])
@@ -310,6 +311,84 @@ class TextAdventure(commands.Cog, name="textadv"):
             color=int(room["color"], base=16)
         )
         await context.send(embed=embed)
+
+    @commands.hybrid_command(
+        name="get",
+        description="Get an item",
+    )
+    @checks.not_blacklisted()
+    @checks.not_dms()
+    async def get(self, context: Context, arg: str):
+        """
+        This is a command that allows you to get an item.
+
+        :param context: The application command context.
+        """
+        state = await get_state(context)
+        try:
+            room = data.get_room_data(state[1])
+        except ValueError:
+            embed = discord.Embed(
+                title="Error!",
+                description="You have somehow landed in an invalid location. Please report this to @AverseABFun.",
+                color=0xE02B2B
+            )
+            await context.send(embed=embed)
+            return
+        try:
+            items = room["items"]
+        except KeyError:
+            embed = discord.Embed(
+                title="Error!",
+                description="There are no items in this room!",
+                color=0xE02B2B
+            )
+            await context.send(embed=embed)
+            return
+        try:
+            item = items[arg]
+        except KeyError:
+            embed = discord.Embed(
+                title="Error!",
+                description="That item isn't in the room!",
+                color=0xE02B2B
+            )
+            await context.send(embed=embed)
+            return
+        try:
+            item = json.load(open(f"items/{item}.json",'r'))
+        except:
+            embed = discord.Embed(
+                title="Error!",
+                description="That item is refrenced but doesn't exist...? Please report this to @AverseABFun.",
+                color=0xE02B2B
+            )
+            await context.send(embed=embed)
+            return
+        if item["type"] == "weapon":
+            print(state)
+            inventory = state[2]
+            inventory = json.loads(inventory)
+            print(inventory)
+            if item["attack"]>=inventory["attack"]:
+                inventory["attack"] = item["attack"]
+                inventory["defense"] = item["defense"]
+                inventory["reload"] = item["reload"]
+                await db_manager.update_state(context.author.id, state[1], json.dumps(inventory))
+                embed = discord.Embed(
+                    description="You get the weapon and equip it. You feel stronger now.",
+                    color=0xE02B2B
+                )
+                await context.send(embed=embed)
+                return
+            else:
+                embed = discord.Embed(
+                    description="You have a better weapon.",
+                    color=0xE02B2B
+                )
+                await context.send(embed=embed)
+                return
+                        
 
 
 async def setup(bot):
